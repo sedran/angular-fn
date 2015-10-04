5483f292e383b698b7caf643f2e6d7a015abfc6a
Serdar Kuzucu <serdarkuzucu13@gmail.com>
2015-10-05 01:44:04 +0300

    Add more unit tests to verify that custom templates are working.

--- test/block-ui-test.js
+++ test/block-ui-test.js
@@ -1,63 +1,171 @@
 describe('fn-block-ui', function() {
-    var compile, scope, directiveElem, blockUiConstants, blockUiService;
+    var compile, scope, timeout;
     
     beforeEach(function(){
         module('fn.block-ui');
 
-        inject(function($compile, $rootScope, fnBlockUiConstants, fnBlockUi){
+        inject(function($compile, $rootScope, $timeout){
             compile = $compile;
             scope = $rootScope.$new();
-            blockUiConstants = fnBlockUiConstants;
-            blockUiService = fnBlockUi;
+            timeout = $timeout;
         });
-
-        directiveElem = getCompiledElement();
     });
     
     afterEach(function () {
         scope.$destroy();
+        
+        // Clear any timeout
+        try {
+            timeout.verifyNoPendingTasks();
+        } catch ( aException ) {
+            timeout.flush();
+        }
     });
     
-    function getCompiledElement(){
-        var element = angular.element('<div fn-block-ui="testing"><p>Something here.</p></div>');
-        var compiledElement = compile(element)(scope);
-        scope.$digest();
-        return compiledElement;
-    }
+    describe('core tests', function () {
+        var directiveElem, blockUiConstants, blockUiService;
     
-    it('should have block-ui class', function () {
-        expect(directiveElem.hasClass(blockUiConstants.elementClass)).toEqual(true);
+        beforeEach(function(){
+            inject(function(fnBlockUiConstants, fnBlockUi){
+                blockUiConstants = fnBlockUiConstants;
+                blockUiService = fnBlockUi;
+            });
+
+            directiveElem = getCompiledElement();
+        });
+
+        function getCompiledElement(){
+            var element = angular.element('<div fn-block-ui="testing"><p id="userContent">Something here.</p></div>');
+            var compiledElement = compile(element)(scope);
+            scope.$digest();
+            return compiledElement;
+        }
+
+        it('should have block-ui class', function () {
+            expect(directiveElem.hasClass(blockUiConstants.elementClass)).toEqual(true);
+        });
+
+        it('should have container element', function () {
+            var elements = directiveElem[0].querySelectorAll('.' + blockUiConstants.containerClass);
+
+            expect(elements).toBeDefined();
+            expect(elements.length).toEqual(1);
+        });
+
+        it('should not be visible initially', function () {
+            expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(false);
+        });
+
+        it('should have visible class when service is called', function () {
+            expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(false);
+
+            blockUiService('testing');
+            scope.$digest();
+
+            expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(true);
+        });
+
+        it('should not have visible class when service return function is called', function () {
+            expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(false);
+
+            var unblockFunction = blockUiService('testing');
+            scope.$digest();
+            expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(true);
+
+            unblockFunction();
+            scope.$digest();
+            expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(false);
+        });
+        
+        it('should have visible class when service is called with timeout', function () {
+            expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(false);
+
+            // Block for 5 seconds
+            blockUiService('testing', 5000);
+            scope.$digest();
+
+            expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(true);
+        });
+        
+        it('should not have visible class after timeout', function () {
+            expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(false);
+
+            // Block for 5 seconds
+            blockUiService('testing', 5000);
+            scope.$digest();
+            
+            // Simulate waiting for 5 seconds
+            timeout.flush(5000);
+
+            expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(false);
+        });
+        
+        it('should not override user content', function () {
+            expect(directiveElem.find('p').attr('id')).toEqual('userContent');
+            expect(directiveElem.find('p').text()).toEqual('Something here.');
+        });
     });
     
-    it('should have container element', function () {
-        var elements = directiveElem[0].querySelectorAll('.' + blockUiConstants.containerClass);
-        
-        expect(elements).toBeDefined();
-        expect(elements.length).toEqual(1);
-    });
+    describe('customization tests', function () {
+        var blockUiConstants, blockUiService, templateCache;
+        var blockUiConstantsCache;
     
-    it('should not be visible initially', function () {
-        expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(false);
-    });
-    
-    it('should have visible class when service is called', function () {
-        expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(false);
+        beforeEach(function(){
+            inject(function(fnBlockUiConstants, fnBlockUi, $templateCache){
+                blockUiConstants = fnBlockUiConstants;
+                blockUiService = fnBlockUi;
+                templateCache = $templateCache;
+                
+                // Cache attributes because they may change.
+                blockUiConstantsCache = angular.copy(blockUiConstants);
+            });
+        });
         
-        blockUiService('testing');
-        scope.$digest();
+        afterEach(function () {
+            // Put cached attributes back.
+            angular.copy(blockUiConstantsCache, blockUiConstants);
+        });
         
-        expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(true);
-    });
-    
-    it('should not have visible class when service return function is called', function () {
-        expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(false);
+        it('should use template from attr', function () {
+            templateCache.put('customTemplate.html', '<div id="customTemplateId">Custom Template</div>');
+            
+            var element = '<div fn-block-ui="testing" fn-block-ui-template="customTemplate.html"></div>';
+            element = compile(element)(scope);
+            scope.$digest();
+            
+            expect(element[0].querySelector('#customTemplateId')).toBeDefined();
+            
+            var customTemplate = angular.element(element[0].querySelector('#customTemplateId'));
+            expect(customTemplate.text()).toEqual('Custom Template');
+        });
         
-        var unblockFunction = blockUiService('testing');
-        scope.$digest();
-        expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(true);
+        it('should use default message in custom template', function () {
+            blockUiConstants.message = 'Hello';
+            templateCache.put('customTemplate.html', '<div id="customTemplateId">{{instance.message}} World!</div>');
+            
+            var element = '<div fn-block-ui="testing" fn-block-ui-template="customTemplate.html"></div>';
+            element = compile(element)(scope);
+            scope.$digest();
+            
+            expect(element[0].querySelector('#customTemplateId')).toBeDefined();
+            
+            var customTemplate = angular.element(element[0].querySelector('#customTemplateId'));
+            expect(customTemplate.text()).toEqual('Hello World!');
+        });
         
-        unblockFunction();
-        scope.$digest();
-        expect(directiveElem.hasClass(blockUiConstants.visibleClass)).toEqual(false);
+        it('should not use default message in custom template if it is defined in directive', function () {
+            blockUiConstants.message = 'Hello';
+            templateCache.put('customTemplate.html', '<div id="customTemplateId">{{instance.message}} World!</div>');
+            
+            var element = '<div fn-block-ui="testing" fn-block-ui-template="customTemplate.html" fn-block-ui-message="Hi"></div>';
+            element = compile(element)(scope);
+            scope.$digest();
+            
+            expect(element[0].querySelector('#customTemplateId')).toBeDefined();
+            
+            var customTemplate = angular.element(element[0].querySelector('#customTemplateId'));
+            expect(customTemplate.text()).not.toEqual('Hello World!');
+            expect(customTemplate.text()).toEqual('Hi World!');
+        });
     });
 });
